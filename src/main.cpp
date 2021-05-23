0168e413a43c4b8d7799f45f5403d1075d124bfd
florisdh <florisdh@gmail.com>
2021-05-24 00:04:28 +0200

    Sleeping multiple times before turning on

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
 
+// Include API-Headers
+extern "C" {
+  #include "user_interface.h"
+}
+
+// RTC-MEM adresses for saving timer
+#define RTC_WAKE_COUNT 67
+
+// GPIO pin connected to pump
+#define PUMP_PIN 4
+
+// How long the pump should turn on
+#define POWER_DURATION 3000
+
+// How long to sleep (max is 72 minutes) (36e8 is 1h) (60e6 is 1m)
+#define SLEEP_DURATION 36e8
+
+// Every how many times sleeping it should turn on
+#define SLEEP_INTERVAL 2
+
+uint32_t sleepCount;
+
 void setup() {
+
+  // Bump sleep counter
+  system_rtc_mem_read(RTC_WAKE_COUNT, &sleepCount, 4);
+  sleepCount++;
+  system_rtc_mem_write(RTC_WAKE_COUNT, &sleepCount, 4);
+
+  // Turn on LED
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(4, OUTPUT);
-}
-
-void loop() {
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(4, LOW);
 
-  delay(3000);
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(4, HIGH);
+  // Turn on pump
+  if (sleepCount % SLEEP_INTERVAL == 0) {
+    pinMode(PUMP_PIN, OUTPUT);    
+    digitalWrite(PUMP_PIN, LOW);
+    delay(POWER_DURATION);
+  }
 
-  ESP.deepSleep(30e6);
+  ESP.deepSleep(SLEEP_DURATION);
 }
+
+void loop() {}
